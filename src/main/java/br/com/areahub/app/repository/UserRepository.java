9b82d1482b1d335352d8e5a6150e51129c2f311c
Hugo Silva <jose.silva@extreme.digital>
2025-06-16 16:19:31 -0300

    feat: Create UserRepository interface for user data access

---
+++ src/main/java/br/com/areahub/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.areahub.app.repository;
+
+import br.com.areahub.app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
